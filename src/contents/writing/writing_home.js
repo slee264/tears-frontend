aa5e9d67a8ee44ca943a99cb96e9ab97cb10e6d2
Joseph Lee <leethfc11@gmail.com>
2021-11-17 21:53:56 -0500

    Feature: adding new write and working out the save/confirm/close/discard mechanism

--- src/contents/writing/writing_home.js
+++ src/contents/writing/writing_home.js
@@ -1,22 +1,31 @@
 import React, { useState, useRef, useEffect } from 'react';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
+
+import Alert from '@mui/material/Alert';
 import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
+import { CardActionArea } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import CloseIcon from '@mui/icons-material/Close';
+import Collapse from '@mui/material/Collapse';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
-import { CardActionArea } from '@mui/material';
+import Fade from '@mui/material/Fade';
+import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
+import Modal from '@mui/material/Modal';
+import { positions } from '@mui/system';
 import TextField from '@mui/material/TextField';
-import Icon from '@mui/material/Icon';
+import Typography from '@mui/material/Typography';
 import Slide from '@mui/material/Slide';
-import { server } from '../../axios';
+import Snackbar from '@mui/material/Snackbar';
 
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+import { server } from '../../axios';
 import "@fontsource/roboto"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
@@ -29,6 +38,14 @@ export default function WritingHome(props){
   const [dialogue_open, setDialogueOpen] = React.useState(false);
   const [writes_list, setWritesList] = React.useState([]);
   const [dialogue, setDialogue] = React.useState({title: '', body: ''});
+  const [dialogue_edit, setDialogueEdit] = React.useState(false);
+
+  const [new_dialogue_open, setNewDialogueOpen] = React.useState(false);
+  const [confirm_modal_open, setConfirmModalOpen] = React.useState(false);
+  const [alert_open, setAlertOpen] = React.useState(false);
+  const [confirm_alert, setConfirmAlert] = React.useState(false);
+  const [save_success, setSaveSuccess] = React.useState(false);
+
   const [dialogue_changed, setDialogueChanged] = React.useState(false);
 
   useEffect(() => {
@@ -94,20 +111,15 @@ export default function WritingHome(props){
     setBackdropOpen(!backdrop_open);
   };
 
-  const handleClickNew = () => {
+  const handleClickWrite = (e) => {
+    setDialogue({id: e.id, title: e.title, body: e.body});
+    setDialogueChanged(false);
     setDialogueOpen(true);
-    setBackdropOpen(false)
   }
 
   const handleSave = async () => {
-    let result = null;
-    if(dialogue_changed && dialogue.id) {
-      console.log('changed');
-      const result = await server.patch('/writes/' + dialogue.id, {title: dialogue_title_ref.current.value, content: dialogue_body_ref.current.value});
-    }else if(dialogue_changed){
-      console.log('new');
-      const result = await server.post('/writes', {title: dialogue_title_ref.current.value, body: dialogue_body_ref.current.value});
-    }
+    console.log('changed');
+    const result = await server.patch('/writes/' + dialogue.id, {title: dialogue_title_ref.current.value, content: dialogue_body_ref.current.value});
     fetchWrites();
     handleDiscard();
   }
@@ -116,12 +128,17 @@ export default function WritingHome(props){
     setDialogueOpen(false);
     setDialogue({title: '', body: ''});
     setDialogueChanged(false);
+    setDialogueEdit(false);
   }
 
-  const handleClickWrite = (e) => {
-    setDialogue({id: e.id, title: e.title, body: e.body});
-    setDialogueChanged(false);
-    setDialogueOpen(true);
+  const handleEdit = () => {
+    if(dialogue_edit){
+      let temp_dialogue = dialogue;
+      dialogue.title = dialogue_title_ref.current.value;
+      dialogue.body = dialogue_body_ref.current.value;
+      setDialogue(temp_dialogue);
+    }
+    setDialogueEdit(!dialogue_edit);
   }
 
   const handleDelete = async () => {
@@ -129,8 +146,85 @@ export default function WritingHome(props){
     if(result) fetchWrites();
   }
 
+  const handleClickNew = () => {
+    setNewDialogueOpen(true);
+    setBackdropOpen(false)
+  }
+
+  const handleNewDiscard = () => {
+    setNewDialogueOpen(false);
+    setAlertOpen(false);
+  }
+
+  const handleNewSave = async () => {
+    const title = dialogue_title_ref.current.value;
+    const body = dialogue_body_ref.current.value;
+    if(body.length == 0){
+      console.log("body empty!!");
+      setConfirmAlert(false);
+      setAlertOpen(true);
+    }else {
+      const result = await server.post('/writes', {title: title.length == 0 ? 'No Title' : title, body: body});
+      fetchWrites();
+      setConfirmAlert(false);
+      setAlertOpen(false);
+      setDialogueChanged(false);
+      setNewDialogueOpen(false);
+      setSaveSuccess(true);
+    }
+  }
+
+  const handleClose = () => {
+    if(dialogue_changed && dialogue_body_ref.current.value.length > 0) {
+      setConfirmAlert(true);
+      setAlertOpen(true);
+    }else {
+      setAlertOpen(false);
+      setNewDialogueOpen(false);
+    }
+  }
+
   const textOnChange = (event) => {
     setDialogueChanged(true);
+  }
+
+  const StatusAlert = () => {
+
+    if(confirm_alert) {
+      return (
+        <Alert
+          variant='filled'
+          severity="warning"
+          sx={{borderRadius: 0}}
+          action={
+            <IconButton aria-label="close" color="inherit" size="small"
+              onClick={() => {
+                setAlertOpen(false);
+              }}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }>
+          You should save it first!
+        </Alert>
+      )
+    } else {
+      return (
+        <Alert
+          variant='filled'
+          severity="error"
+          sx={{borderRadius: 0}}
+          action={
+            <IconButton aria-label="close" color="inherit" size="small"
+              onClick={() => {
+                setAlertOpen(false);
+              }}>
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }>
+          Oh no! You need to write something!
+        </Alert>
+      )
+    }
   }
 
   const theme = createTheme({
@@ -164,24 +258,80 @@ export default function WritingHome(props){
           TransitionComponent={Transition}
           fullWidth>
           <DialogTitle>
-            <TextField defaultValue={dialogue.title} onChange={textOnChange} inputRef={dialogue_title_ref} id="dialogue_title" label="Title" variant="standard" fullWidth />
+            {dialogue_edit?
+              <TextField defaultValue={dialogue.title} onChange={textOnChange} inputRef={dialogue_title_ref} id="dialogue_title" label="Title" variant="standard" fullWidth />
+              :<Typography> {dialogue.title} </Typography>}
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+              }}>
+              <CloseIcon />
+            </IconButton>
           </DialogTitle>
           <DialogContent>
-            <TextField defaultValue={dialogue.body} onChange={textOnChange} inputRef={dialogue_body_ref} id="dialogue_body" placeholder="Write here!" multiline rows={20} fullWidth />
+            {dialogue_edit?
+              <TextField defaultValue={dialogue.body} onChange={textOnChange} inputRef={dialogue_body_ref} id="dialogue_body" placeholder="Write here!" multiline rows={20} fullWidth />
+            : <Typography sx={{whiteSpace: 'pre-wrap'}}> {dialogue.body} </Typography>}
           </DialogContent>
           <DialogActions>
             {dialogue.id ?
             <Button sx={{color: '#d62828'}} onClick={handleDelete}>
               Delete
             </Button> : null}
-            <Button autoFocus onClick={handleSave}>
-              Save Changes
+            {dialogue_edit ?
+              <Button autoFocus onClick={handleEdit}>
+              View
+              </Button>
+            :
+              <Button autoFocus onClick={handleEdit}>
+              Edit
+              </Button>
+            }
+          </DialogActions>
+        </Dialog>
+        <Dialog
+          open={new_dialogue_open}
+          TransitionComponent={Transition}
+          fullWidth>
+          <DialogTitle>
+            <TextField onChange={textOnChange} inputRef={dialogue_title_ref} id="dialogue_title" label="Title" variant="standard" fullWidth />
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                position: 'absolute',
+                right: 8,
+                top: 8,
+              }}>
+              <CloseIcon />
+            </IconButton>
+          </DialogTitle>
+          <DialogContent>
+            <TextField onChange={textOnChange} inputRef={dialogue_body_ref} id="dialogue_body" placeholder="Write here!" multiline rows={20} fullWidth />
+          </DialogContent>
+          <DialogActions>
+            <div style={{flex: '1 0 0'}}>
+            <Button onClick={handleNewSave}>
+              Save
             </Button>
-            <Button autoFocus onClick={handleDiscard}>
+            </div>
+            <Button onClick={handleNewDiscard}>
               Discard
             </Button>
           </DialogActions>
+          <Collapse in={alert_open}>
+            <StatusAlert/>
+          </Collapse>
         </Dialog>
+        <Snackbar anchorOrigin={{vertical: 'bottom', horizontal: 'center'}} open={save_success} autoHideDuration={6000} onClose={() => setSaveSuccess(false)}>
+          <Alert onClose={() => setSaveSuccess(false)} severity='success' sx={{ width: '100%'}}>
+            Saved! Well done!
+          </Alert>
+        </Snackbar>
       </ThemeProvider>
     </div>
   )
