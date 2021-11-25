db8dde0b074f5c78862a44cfd19171fa0f890ae2
Joseph Lee <leethfc11@gmail.com>
2021-11-24 20:57:48 -0500

    Fixing tests

--- src/components/__tests__/writing_home.test.js
+++ src/components/__tests__/writing_home.test.js
@@ -5,10 +5,10 @@ import WritingHome from '../writing/writing_home';
 
 describe('writing home', () => {
   it("renders default state", () => {
-    const { getByTestId } = render(<WritingHome />);
+    const container = render(<WritingHome />);
 
     //introductory backdrop opens automatically when you load the page
-    const backdrop = getByTestId('introductory-backdrop');
+    const backdrop = container.getByTestId('introductory-backdrop');
 
     //once you click the backdrop, backdrop disappears
     //and then load appropriate cards.
@@ -17,37 +17,37 @@ describe('writing home', () => {
 
   it("backdrop disappears when you click it, and loads appropriate cards", () => {
 
-    const { getByTestId } = render(<WritingHome />);
+    const container = render(<WritingHome />);
 
     //introductory backdrop opens automatically when you load the page
-    const backdrop = getByTestId('introductory-backdrop');
+    const backdrop = container.getByTestId('introductory-backdrop');
 
     //once you click the backdrop, backdrop disappears
     //and then load appropriate cards.
     fireEvent.click(backdrop);
 
     //now should appear the "click new" card (along with other dummy cards, in the future)
-    const add_card = getByTestId('card-add-new');
+    const add_card = container.getByTestId('card-add-new');
   })
 
   it("loads a new dialogue with title and body textfields when you click the add new card", () => {
-    const { getByTestId } = render(<WritingHome />);
+    const container = render(<WritingHome />);
 
     //introductory backdrop opens automatically when you load the page
-    const backdrop = getByTestId('introductory-backdrop');
+    const backdrop = container.getByTestId('introductory-backdrop');
 
     //once you click the backdrop, backdrop disappears
     //and then load appropriate cards.
     fireEvent.click(backdrop);
 
-    const add_card_action_area = getByTestId("card-action-area-add-new");
+    const add_card_action_area = container.getByTestId("card-action-area-add-new");
 
     //you press the add new card
     fireEvent.click(add_card_action_area);
 
     //now appears empty title and body textfields
-    const new_title_textfield = getByTestId("new-title-textfield");
-    const new_body_textfield = getByTestId("new-body-textfield");
+    const new_title_textfield = container.getByTestId("new-title-textfield");
+    const new_body_textfield = container.getByTestId("new-body-textfield");
 
     console.log(new_title_textfield);
   })
